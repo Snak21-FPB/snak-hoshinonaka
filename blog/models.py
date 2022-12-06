0c9786275264a9a30dfb0dc18bd38abbdf249fea
Snak <s.nakajima@4nonome.com>
2022-12-06 13:56:30 +0900

    add superuser

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,28 @@ from django.contrib.auth.models import (
 
 
 class WriterManager(BaseUserManager):
-    pass
+    def create_user(self, username, email, permission=1, password=None):
+        if not username:
+            raise ValueError("Writer must have an username.")
+        if not email:
+            raise ValueError("Writer must have an email address.")
+
+        user = self.model(
+            username=username, email=self.normalize_email(email), permission=permission
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password):
+        user = self.create_user(
+            username=username,
+            email=self.normalize_email(email),
+            password=password,
+            permission=9,
+        )
+        user.save(using=self._db)
+        return user
 
 
 class Writer(AbstractBaseUser, PermissionsMixin):
@@ -15,9 +36,30 @@ class Writer(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(verbose_name="メールアドレス", unique=True)
     password = models.CharField(verbose_name="パスワード", max_length=256)
     username = models.SlugField(verbose_name="ユーザー名", max_length=32, unique=True)
-    name = models.CharField(verbose_name="名前", max_length=128)
+    name = models.CharField(verbose_name="名前", max_length=128, blank=True, null=True)
     permission = models.PositiveSmallIntegerField(verbose_name="権限", default=1)
     last_login = models.DateTimeField(verbose_name="最終ログイン日時", blank=True, null=True)
     login_num = models.IntegerField(verbose_name="ログイン回数", default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    objects = WriterManager()
+
+    USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def is_active(self):
+        return self.permission >= 1
+
+    @property
+    def is_staff(self):
+        return self.permission >= 2
+
+    @property
+    def is_superuser(self):
+        return self.permission >= 9
--- snak_hoshinonaka/settings.py
+++ snak_hoshinonaka/settings.py
@@ -106,8 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
-LANGUAGE_CODE = "en-us"
-TIME_ZONE = "UTC"
+LANGUAGE_CODE = "ja"
+TIME_ZONE = "Asia/Tokyo"
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
@@ -116,3 +116,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = "/static/"
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ["static"]))
+
+AUTH_USER_MODEL = "blog.Writer"
